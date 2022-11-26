51241fd3e7754fde13a51757475e602c7f4a90f4
JDRjuju <94639280+JDRjuju@users.noreply.github.com>
2022-11-26 14:28:29 +0100

    feat : added rollMany and beforeEach

--- src/___test___/bowlingKata.test.js
+++ src/___test___/bowlingKata.test.js
@@ -2,24 +2,29 @@ const bowlingKata = require('../bowlingKata');
 
 let bowling;
 
-// Test 1.3
+describe("Game Bowling Kata", function () {
 
-describe("Initialize Bowling Kata", function () {
+   beforeEach(function () {
+      bowling = new bowlingKata();
+   });
+
+  // Test 1.3
   it("Can roll", function () {
-    bowling = new bowlingKata();
-    for(i = 0; i < 20; i++){
-      bowling.roll(0);
-    }
+    rollMany(0, 20);
     expect(bowling.score()).toBe(0);
   });
 
+  // Test 2.1
   it("Can roll all ones", function() {
-    bowling = new bowlingKata();
-    for(i = 0; i < 20; i++){
-      bowling.roll(1);
-    }
+    rollMany(1, 20);
     expect(bowling.score()).toBe(20);
   });
+
+  let rollMany = function (pins, rolls) {
+    for (var i = 0; i < rolls; i++) {
+      bowling.roll(pins);
+    }
+  };
 });
 
 
