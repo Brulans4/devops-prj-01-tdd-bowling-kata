21569d14c1da7bf4590bdcde8af410466ee25980
Brulans4 <118511976+Brulans4@users.noreply.github.com>
2022-11-26 15:11:07 +0100

    Clean up comments

--- src/___test___/bowlingKata.test.js
+++ src/___test___/bowlingKata.test.js
@@ -7,19 +7,19 @@ describe("Game Bowling Kata", function () {
     bowling = new bowlingKata();
   });
 
-  // Test 1.3
+  // Test 1
   it("Can roll", function () {
     rollMany(0, 20);
     expect(bowling.score()).toBe(0);
   });
 
-  // Test 2.1
+  // Test 2
   it("Can roll all ones", function () {
     rollMany(1, 20);
     expect(bowling.score()).toBe(20);
   });
 
-  // Test 3.2
+  // Test 3
   it("Can roll a spare", function () {
     bowling.roll(5);
     bowling.roll(5); //spare
@@ -28,18 +28,18 @@ describe("Game Bowling Kata", function () {
     expect(bowling.score()).toBe(16);
   });
 
-  // Test 4.1
+  // Test 4
   it("Can roll a strike", function () {
-    bowling.roll(10);
+    bowling.roll(10); //strink
     bowling.roll(4);
     bowling.roll(3);
     rollMany(0, 16);
     expect(bowling.score()).toBe(24);
   });
 
-  // Test 5.1
+  // Test 5
   it("Can roll a perfect game", function () {
-    rollMany(10, 12);
+    rollMany(10, 12); //perfect game
     expect(bowling.score()).toBe(300);
   });
 
